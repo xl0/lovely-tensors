3604c227f990e76b6f2d894e1a2a9ed4cdeaffed
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2022-12-18 23:22:23 +0700

    Update config manager

--- lovely_tensors/utils/config.py
+++ lovely_tensors/utils/config.py
@@ -11,62 +11,67 @@ from contextlib import contextmanager
 from lovely_numpy import config as np_config
 
 # %% ../../nbs/03_utils.config.ipynb 4
-_defaults = SimpleNamespace(
-    precision     = 3,    # Digits after `.`
-    threshold_max = 3,    # .abs() larger than 1e3 -> Sci mode
-    threshold_min = -4,   # .abs() smaller that 1e-4 -> Sci mode
-    sci_mode      = None, # Sci mode (2.3e4). 
-    indent        = 2,    # Indent for .deeper()
-    color         = True, # ANSI colors in text
-    deeper_width  = 9,    # For .deeper, width per level
-)
+class Config(SimpleNamespace):
+    "Config"
+    def __init__(self,
+            precision     = 3,    # Digits after `.`
+            threshold_max = 3,    # .abs() larger than 1e3 -> Sci mode
+            threshold_min = -4,   # .abs() smaller that 1e-4 -> Sci mode
+            sci_mode      = None, # Sci mode (2.3e4). None=auto
+            indent        = 2,    # Indent for .deeper()
+            color         = True, # ANSI colors in text
+            deeper_width  = 9,    # For .deeper, width per level
 
+    ):
+        super().__init__(**{k:v for k,v in locals().items() if k not in ["self", "__class__"]})
+
+_defaults = Config()
 _config = copy(_defaults)
 
-# %% ../../nbs/03_utils.config.ipynb 5
-# Allows passing None as an argument to reset to defaults
+# %% ../../nbs/03_utils.config.ipynb 7
+# Allows passing None as an argument to reset the 
 class _Default():
     def __repr__(self):
         return "Ignore"
-Default = _Default()
-D = TypeVar("Default")
+D = _Default()
+Default = TypeVar("Default")
 
-# %% ../../nbs/03_utils.config.ipynb 6
-def set_config( precision       :Optional[Union[D, int]] =Default,  # Digits after `.`
-                threshold_min   :Optional[Union[D,int]]  =Default,  # .abs() larger than 1e3 -> Sci mode
-                threshold_max   :Optional[Union[D,int]]  =Default,  # .abs() smaller that 1e-4 -> Sci mode
-                sci_mode        :Optional[Union[D,bool]] =Default,  # Sci mode (1.2e3), True, False, None=auto.
-                indent          :Optional[Union[D,bool]] =Default,  # Indent for .deeper()
-                color           :Optional[Union[D,bool]] =Default,  # ANSI colors in text
-                deeper_width     :Optional[Union[D,int]] =Default): # For .deeper, width per level
+# %% ../../nbs/03_utils.config.ipynb 8
+def set_config( precision       :Optional[Union[Default,int]]  =D,
+                threshold_min   :Optional[Union[Default,int]]  =D,
+                threshold_max   :Optional[Union[Default,int]]  =D,
+                sci_mode        :Optional[Union[Default,bool]] =D,
+                indent          :Optional[Union[Default,bool]] =D,
+                color           :Optional[Union[Default,bool]] =D,
+                deeper_width    :Optional[Union[Default,int]]  =D):
 
     "Set config variables"
     args = locals().copy()
     for k,v in args.items():
-        if v != Default:
+        if v != D:
             if v is None:
                 setattr(_config, k, getattr(_defaults, k))
             else:
                 setattr(_config, k, v)
 
-# %% ../../nbs/03_utils.config.ipynb 7
+# %% ../../nbs/03_utils.config.ipynb 9
 def get_config():
     "Get a copy of config variables"
     return copy(_config)
 
-# %% ../../nbs/03_utils.config.ipynb 8
+# %% ../../nbs/03_utils.config.ipynb 10
 @contextmanager
-def config( precision       :Optional[Union[D,int]]  =Default,  # Digits after `.`
-            threshold_min   :Optional[Union[D,int]]  =Default,  # .abs() larger than 1e3 -> Sci mode
-            threshold_max   :Optional[Union[D,int]]  =Default,  # .abs() smaller that 1e-4 -> Sci mode
-            sci_mode        :Optional[Union[D,bool]] =Default,  # Sci mode (1.2e3), True, False, None=auto.
-            indent          :Optional[Union[D,bool]] =Default,  # Indent for .deeper()
-            color           :Optional[Union[D,bool]] =Default,  # ANSI colors in text
-            deeper_width     :Optional[Union[D,int]] =Default): # For .deeper, width per level
+def config( precision       :Optional[Union[Default,int]]  =D,
+            threshold_min   :Optional[Union[Default,int]]  =D,
+            threshold_max   :Optional[Union[Default,int]]  =D,
+            sci_mode        :Optional[Union[Default,bool]] =D,
+            indent          :Optional[Union[Default,bool]] =D,
+            color           :Optional[Union[Default,bool]] =D,
+            deeper_width    :Optional[Union[Default,int]]  =D):
 
     "Context manager for temporarily setting printting options."
     global _config
-    new_opts = { k:v for k, v in locals().items() if v != Default }
+    new_opts = { k:v for k, v in locals().items() if v != D }
     old_opts = copy(get_config().__dict__)
 
     try:
