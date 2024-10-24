f3ea36fdfb71db81d8a95ebcb3a722b8f833979b
Federico Baldassarre <baldassarre.fe@gmail.com>
2024-10-24 10:58:31 +0000

    Enable automatically using import hook and env variable

--- setup.py
+++ setup.py
@@ -1,7 +1,60 @@
 from pkg_resources import parse_version
 from configparser import ConfigParser
+import itertools
+import os.path
 import setuptools
+from distutils.command.build import build
+from setuptools.command.develop import develop
+from setuptools.command.easy_install import easy_install
+from setuptools.command.install_lib import install_lib
 assert parse_version(setuptools.__version__)>=parse_version('36.2')
+
+LOVELY_TENSORS_HOOK_SRC = "lovely_tensors_hook.py"
+LOVELY_TENSORS_HOOK = "lovely_tensors_hook.pth"
+
+
+def create_hook(src_path, dest_path):
+    with open(src_path, 'r') as f:
+        src_code = f.read()
+    with open(dest_path, 'w') as f:
+        print(f"import os; exec({src_code!r})", file=f)
+
+
+class BuildWithHook(build):
+    def run(self):
+        build.run(self)
+        path = os.path.join(os.path.dirname(__file__), LOVELY_TENSORS_HOOK_SRC)
+        dest = os.path.join(self.build_lib, LOVELY_TENSORS_HOOK)
+        self.make_file([path], dest, create_hook, [path, dest])
+
+
+class EasyInstallWithHook(easy_install):
+    def run(self):
+        easy_install.run(self)
+        path = os.path.join(self.build_directory, LOVELY_TENSORS_HOOK)
+        dest = os.path.join(self.install_dir, LOVELY_TENSORS_HOOK)
+        self.copy_file(path, dest)
+
+
+class InstallLibWithHook(install_lib):
+    def run(self):
+        install_lib.run(self)
+        path = os.path.join(self.build_dir, LOVELY_TENSORS_HOOK)
+        dest = os.path.join(self.install_dir, LOVELY_TENSORS_HOOK)
+        self.copy_file(path, dest)
+        self.outputs = [dest]
+
+    def get_outputs(self):
+        return itertools.chain(install_lib.get_outputs(self), self.outputs)
+
+
+class DevelopWithHook(develop):
+    def run(self):
+        develop.run(self)
+        path = os.path.join(self.build_directory, LOVELY_TENSORS_HOOK)
+        dest = os.path.join(self.install_dir, LOVELY_TENSORS_HOOK)
+        self.copy_file(path, dest)
+
 
 # note: all settings are in settings.ini; edit there, not here
 config = ConfigParser(delimiters=['='])
@@ -51,6 +104,14 @@ setuptools.setup(
     entry_points = {
         'console_scripts': cfg.get('console_scripts','').split(),
         'nbdev': [f'{cfg.get("lib_path")}={cfg.get("lib_path")}._modidx:d']
+    },
+    # The import hooks mechanism is inspired by the one used in better_exceptions
+    # https://github.com/Qix-/better-exceptions/blob/f7f1476e57129dc74d241b4377b0df39c37bc8a7/setup.py
+    cmdclass = {
+        'build': BuildWithHook,
+        'easy_install': EasyInstallWithHook,
+        'install_lib': InstallLibWithHook,
+        'develop': DevelopWithHook,
     },
     **setup_cfg)
 
